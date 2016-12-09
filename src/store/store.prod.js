66e4c2f6070c6565401312cdc5b248d0c93b70ad
Cristian Barrientos Montoya <cristian_barrientos23132@elpoli.edu.co>
2016-12-09 07:22:56 -0500

    added store conf for production

--- src/store/store.prod.js
+++ src/store/store.prod.js
@@ -1,15 +1,13 @@
 import {createStore, applyMiddleware} from 'redux';
 import {rootReducer, rootEpic} from './rootReducer';
 import {loadState} from '../middleware/persistentState/persistentState';
-import createLogger from 'redux-logger';
 import persistentMiddleware from '../middleware/persistentState/persistentStateMiddleware';
 import {createEpicMiddleware} from 'redux-observable';
 
 const epicMiddleware = createEpicMiddleware(rootEpic);
-const logger = createLogger();
 const persistent = persistentMiddleware(['form']);
 
-const finalCreateStore = applyMiddleware(epicMiddleware, logger, persistent)(createStore);
+const finalCreateStore = applyMiddleware(epicMiddleware, persistent)(createStore);
 
 function configureStore() {
     const persistedState = loadState();
