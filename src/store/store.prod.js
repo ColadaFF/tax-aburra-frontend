770a8764d884bac03c1f3ae8105133fea22e395f
Cristian Barrientos Montoya <cristian_barrientos23132@elpoli.edu.co>
2016-12-09 07:20:11 -0500

    added store conf for production

--- src/store/store.prod.js
+++ src/store/store.prod.js
@@ -1,3 +1,19 @@
-/**
- * Created by barcode on 10/6/16.
- */
+import {createStore, applyMiddleware} from 'redux';
+import {rootReducer, rootEpic} from './rootReducer';
+import {loadState} from '../middleware/persistentState/persistentState';
+import createLogger from 'redux-logger';
+import persistentMiddleware from '../middleware/persistentState/persistentStateMiddleware';
+import {createEpicMiddleware} from 'redux-observable';
+
+const epicMiddleware = createEpicMiddleware(rootEpic);
+const logger = createLogger();
+const persistent = persistentMiddleware(['form']);
+
+const finalCreateStore = applyMiddleware(epicMiddleware, logger, persistent)(createStore);
+
+function configureStore() {
+    const persistedState = loadState();
+    return finalCreateStore(rootReducer, persistedState);
+}
+
+export default configureStore;
